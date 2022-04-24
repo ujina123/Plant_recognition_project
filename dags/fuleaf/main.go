7fdb5899d2900128a5a8b22ffa070121dfd02178
JngMkk <kjm0817ss@gmail.com>
2022-04-24 17:21:20 +0900

    update

--- dags/fuleaf/main.go
+++ dags/fuleaf/main.go
@@ -12,12 +12,23 @@ import (
 )
 
 type plants struct {
-	name       string
-	info       string
-	waterCycle string
-	waterInfo  string // 물 + info
-	lightInfo  string
-	humidity   string
+	name         string
+	botanyNm     string
+	info         string
+	waterCycle   string
+	waterInfo    string
+	waterExp     string
+	waterExpInfo string
+	light        string
+	lightInfo    string
+	lightExp     string
+	lightExpInfo string
+	humidity     string
+	humidInfo    string
+	humidExp     string
+	humidExpInfo string
+	tempExp      string
+	tempExpInfo  string
 }
 
 func trimSpaceNewlineInString(s string) string {
@@ -27,6 +38,48 @@ func trimSpaceNewlineInString(s string) string {
 
 func replaceHref(s string) string {
 	return strings.ReplaceAll(s, "count", "detail")
+}
+
+func splitC(s string) string {
+	return strings.TrimSpace(strings.Split(s, ":")[1])
+}
+
+func trim(s string) string {
+	return strings.TrimSpace(s)
+}
+
+func split(s, sep string) []string {
+	return strings.Split(s, sep)
+}
+
+func contains(s, sep string) bool {
+	return strings.Contains(s, sep)
+}
+
+func deleteMinus(s string) string {
+	return strings.ReplaceAll(s, "-", "")
+}
+
+func deleteString(s string) string {
+	re := regexp.MustCompile(`\[[^)]*\]`)
+	return re.ReplaceAllString(s, "")
+}
+
+func trimSpace(s string) string {
+	re := regexp.MustCompile(`\s{2,}`)
+	return re.ReplaceAllString(s, " ")
+}
+
+func containString(s string) string {
+	var result string
+	if contains(s, "[관리 제품]") {
+		result = split(s, "[관리 제품]")[0]
+	} else if contains(s, "[관리제품]") {
+		result = split(s, "[관리제품]")[0]
+	} else {
+		result = s
+	}
+	return result
 }
 
 // check error
@@ -63,7 +116,60 @@ func getHrefs() []string {
 	return hrefs
 }
 
-func getInfo(href string, c chan<- plants) {
+func getInfo(s *goquery.Selection, c chan<- plants) {
+	nm := s.Find(".simpleinfo-plantname_korean.mainTitle").Text()
+	inf := trimSpace(trim(trimSpaceNewlineInString(s.Find(".simpleinfo-plantname_intro").Text())))
+	wCycle := trim(s.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(1) > h1").Text())
+	wInfo := trimSpace(trim(s.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(1) > h2").Text()))
+	lgt := trim(s.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(2) > h1").Text())
+	lInfo := trim(s.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(2) > h2").Text())
+	hd := trim(s.Find(".simpleinfo-table > ul > div:nth-child(2) > li:nth-child(1) > h1").Text())
+	hdInfo := trim(s.Find(".simpleinfo-table > ul > div:nth-child(2) > li:nth-child(1) > h2").Text())
+	boName := trim(s.Find(".simpleinfo-table > ul > div:nth-child(2) > li:nth-child(2) > h2").Text())
+	lExp := trim(splitC(s.Find(".plants-raise-section > ul > li:nth-child(1) > h1").Text()))
+	tExp := trim(splitC(s.Find(".plants-raise-section > ul > li:nth-child(2) > h1").Text()))
+	hExp := trim(splitC(s.Find(".plants-raise-section > ul > li:nth-child(3) > h1").Text()))
+	wExp := trim(splitC(s.Find(".plants-raise-section > ul > li:nth-child(4) > h1").Text()))
+
+	lExpStr := s.Find(".plants-raise-section > ul > li:nth-child(1) > h2").Text()
+	lExpStr = strings.Join(strings.Fields(lExpStr), " ")
+	lExpInfo := trimSpace(trim(deleteMinus(deleteString(containString(lExpStr)))))
+
+	tExpInfo := trimSpace(trim(deleteString(strings.Join(strings.Fields(s.Find(".plants-raise-section > ul > li:nth-child(2) > h2").Text()), " "))))
+
+	hExpStr := s.Find(".plants-raise-section > ul > li:nth-child(3) > h2").Text()
+	hExpStr = strings.Join(strings.Fields(hExpStr), " ")
+	hExpInfo := trimSpace(trim(deleteMinus(deleteString(containString(hExpStr)))))
+
+	wExpStr := s.Find(".plants-raise-section > ul > li:nth-child(4) > h2").Text()
+	wExpStr = strings.Join(strings.Fields(wExpStr), " ")
+	wExpInfo := trimSpace(trim(deleteMinus(deleteString(containString(wExpStr)))))
+
+	c <- plants{
+		name:         nm,
+		info:         inf,
+		waterCycle:   wCycle,
+		waterInfo:    wInfo,
+		light:        lgt,
+		lightInfo:    lInfo,
+		humidity:     hd,
+		humidInfo:    hdInfo,
+		botanyNm:     boName,
+		lightExp:     lExp,
+		lightExpInfo: lExpInfo,
+		tempExp:      tExp,
+		tempExpInfo:  tExpInfo,
+		humidExp:     hExp,
+		humidExpInfo: hExpInfo,
+		waterExp:     wExp,
+		waterExpInfo: wExpInfo,
+	}
+}
+
+func getTotalInfo(href string, mainC chan<- []plants) {
+	var pls []plants
+	c := make(chan plants)
+
 	res, err := http.Get(href)
 	checkErr(err)
 	checkCode(res)
@@ -73,36 +179,17 @@ func getInfo(href string, c chan<- plants) {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	nm := doc.Find(".simpleinfo-plantname_korean.mainTitle").Text()
-	inf := trimSpaceNewlineInString(doc.Find(".simpleinfo-plantname_intro").Text())
-	wCycle := doc.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(1) > h1").Text()
-	wInfo := doc.Find(".plants-raise-section > ul > li:nth-child(4) > h1").Text() + " " + doc.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(1) > h2").Text()
-	light := doc.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(2) > h1").Text() + " " + doc.Find(".simpleinfo-table > ul > div:nth-child(1) > li:nth-child(2) > h2").Text()
-	hd := doc.Find(".simpleinfo-table > ul > div:nth-child(2) > li:nth-child(1) > h1").Text()
+	body := doc.Find("#plantDetail__page")
+	body.Each(func(i int, s *goquery.Selection) {
+		go getInfo(s, c)
+	})
 
-	c <- plants{
-		name:       nm,
-		info:       inf,
-		waterCycle: wCycle,
-		waterInfo:  wInfo,
-		lightInfo:  light,
-		humidity:   hd,
-	}
-}
-
-func getTotalInfo(hrefs []string) []plants {
-	var pls []plants
-	c := make(chan plants)
-
-	for _, v := range hrefs {
-		go getInfo(v, c)
-	}
-
-	for i := 0; i < len(hrefs); i++ {
+	for i := 0; i < body.Length(); i++ {
 		pl := <-c
 		pls = append(pls, pl)
 	}
-	return pls
+
+	mainC <- pls
 }
 
 func writeCsv(p []plants) {
@@ -113,19 +200,37 @@ func writeCsv(p []plants) {
 
 	defer w.Flush()
 
-	headers := []string{"name", "info", "waterCycle", "waterInfo", "lightInfo", "humidity"}
+	headers := []string{"name", "botanyNm", "info",
+		"waterCycle", "waterInfo", "waterExp", "waterExpInfo",
+		"light", "lightInfo", "lightExp", "lightExpInfo",
+		"humidity", "humidInfo", "humidExp", "humidExpInfo",
+		"tempExp", "tempExpInfo"}
 	wErr := w.Write(headers)
 	checkErr(wErr)
 
 	for _, v := range p {
-		slice := []string{v.name, v.info, v.waterCycle, v.waterInfo, v.lightInfo, v.humidity}
+		slice := []string{v.name, v.botanyNm, v.info,
+			v.waterCycle, v.waterInfo, v.waterExp, v.waterExpInfo,
+			v.light, v.lightInfo, v.lightExp, v.lightExpInfo,
+			v.humidity, v.humidInfo, v.humidExp, v.humidExpInfo,
+			v.tempExp, v.tempExpInfo}
 		jwErr := w.Write(slice)
 		checkErr(jwErr)
 	}
 }
 
 func main() {
+	var pls []plants
 	hrefs := getHrefs()
-	info := getTotalInfo(hrefs)
-	writeCsv(info)
+	c := make(chan []plants)
+
+	for _, v := range hrefs {
+		go getTotalInfo(v, c)
+	}
+
+	for i := 0; i < len(hrefs); i++ {
+		pl := <-c
+		pls = append(pls, pl...)
+	}
+	writeCsv(pls)
 }
