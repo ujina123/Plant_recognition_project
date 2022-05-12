ea6fd6ec4c8e5a653e7dedd5c7fb84b50bb65a76
Ubuntu <ubuntu@ip-172-31-36-191.ap-northeast-1.compute.internal>
2022-05-12 18:34:52 +0900

    signup update

--- dags/airflow.py
+++ dags/airflow.py
@@ -44,17 +44,7 @@ getWeather = BashOperator(
 
 checkHDFS = BashOperator(
     task_id='checkHDFS',
-    bash_command="""
-                    if [ -z $(jps | grep "ResourceManager") ] || [ -z $(jps | grep NodeManager) ]
-                    then echo "Not Running"; start-yarn.sh
-                    else echo "Running"
-                    fi
-
-                    if [ -z $(jps | grep "NameNode") ] || [ -z $(jps | grep "DataNode") ]
-                    then echo "Not Running"; start-dfs.sh
-                    else echo "Running"
-                    fi
-                """,
+    bash_command="sh /home/ubuntu/finalproject/dags/shell/checkhdfs.sh",
     dag=dag
 )
 
