5715a1cacf860ae98b793de1299a293eef50e7ba
JngMkk <kjm0817ss@gmail.com>
2022-05-13 16:54:38 +0900

    어플!

---
+++ Plantdoctor/App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://plantdoctor.shop/' }} style={{ marginTop: 20 }} />;
+  }
+}
